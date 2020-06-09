0ae550694840c106c165898687f1af5e1df36870
Shahab Dogar <shahab96@gmail.com>
2020-06-08 18:54:31 -0700

    Update to cdk 1.44

--- lib/mc_server_infra-stack.ts
+++ lib/mc_server_infra-stack.ts
@@ -9,12 +9,12 @@ export class McServerInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, "MinecraftVPC", {
-      maxAzs: 1,
+    const vpc = new ec2.Vpc(this, "VPC", {
+      maxAzs: 1,      
     });
     const machineImage = new ec2.AmazonLinuxImage();
-    const instanceType = ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3_AMD, ec2.InstanceSize.MICRO);
-    const securityGroup = new ec2.SecurityGroup(this, "MinecraftSecurityGroup", {
+    const instanceType = ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3_AMD, ec2.InstanceSize.MEDIUM);
+    const securityGroup = new ec2.SecurityGroup(this, "SecurityGroup", {
       vpc,
     });
 
@@ -32,14 +32,23 @@ export class McServerInfraStack extends cdk.Stack {
       keyName: "MinecraftKeyPair",
     });
 
-    const target = route53.RecordTarget.fromIpAddresses(this.serverInstance.instancePublicIp);
-    const zone = route53.HostedZone.fromHostedZoneAttributes(this, "MinecraftServerDomainHostedZone", {
+    const zone = route53.PublicHostedZone.fromHostedZoneAttributes(this, "HostedZone", {
       hostedZoneId: "Z2GCKEQH874TQR",
       zoneName: "shahab96.com",
     });
+    const hostedZone = new route53.PublicHostedZone(this, "MinecraftHostedZone", {
+      zoneName: `minecraft.${zone.zoneName}`,
+    });
+    new route53.RecordSet(this, "DelegationRecord", {
+      recordType: route53.RecordType.NS,
+      target: route53.RecordTarget.fromValues(...hostedZone.hostedZoneNameServers!),
+      zone,
+      recordName: hostedZone.zoneName,
+    });
+    const target = route53.RecordTarget.fromIpAddresses(this.serverInstance.instancePublicIp);
     new route53.ARecord(this, "MinecraftServerDNSRecord", {
       recordName: "minecraft.shahab96.com",
-      zone,
+      zone: hostedZone,
       target,
     });
   }
